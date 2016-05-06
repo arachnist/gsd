0127797a82e317d120a481b89dd5f8810d9e8bfc
Robert Gerus <arachnist@i.am-a.cat>
2016-05-06 19:27:11 +0200

    We're now reading the configuration file, hopefully.

--- gsd.go
+++ gsd.go
@@ -11,11 +11,28 @@ package main
 import "C"
 
 import (
-	// "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 	"sync"
 	"time"
 )
 
+type statusbarItem struct {
+	Name   string            `yaml:"name"`
+	Period int               `yaml:"period"`
+	Type   string            `yaml:"type"`
+	Args   map[string]string `yaml:"args"`
+}
+
+type gsdConfig struct {
+	Separator string          `yaml:"string"`
+	Items     []statusbarItem `yaml:"items"`
+}
+
+var config gsdConfig
 var statusLock sync.Mutex
 var statusbar []string
 
@@ -24,6 +41,7 @@ func updateStatusbar(pos int, text string) {
 	defer statusLock.Unlock()
 
 	statusbar[pos] = text
+	C.xname(C.CString(strings.Join(statusbar, config.Separator)))
 }
 
 func spawnUpdater(pos, period int, args map[string]string, f func(map[string]string) string) {
@@ -44,5 +62,17 @@ func fileReader(args map[string]string) string {
 }
 
 func main() {
-	C.xname(C.CString("placeholder"))
+	if len(os.Args) < 2 {
+		log.Fatalln("Usage:", os.Args[0], "<configuration file>")
+	}
+
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalln("Error reading configuration file:", err)
+	}
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		log.Fatalln("Error parsing configuration file:", err)
+	}
+
 }
